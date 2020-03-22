451ddde0ee5f27cfa104dd682364aa30500644c9
Kevin <kevin.m.guertin@gmail.com>
2020-03-21 22:33:04 -0400

    add abiity ro delete items and clear all tasks, as well as filter tasks

--- task-list-tutorial/app.js
+++ task-list-tutorial/app.js
@@ -13,6 +13,15 @@ loadEventListeners();
 function loadEventListeners() {
   //Add Task Event
   form.addEventListener("submit", addTask);
+
+  //Remove Task Event
+  taskList.addEventListener("click", removeTask);
+
+  //Clear Task Event
+  clearBtn.addEventListener("click", clearTasks);
+
+  //Filter Tasks
+  filter.addEventListener("keyup", filterTasks);
 }
 
 //Add Task
@@ -43,4 +52,37 @@ function addTask(e) {
   taskInput.value = "";
 
   e.preventDefault();
+}
+
+// Remove Task
+
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains("delete-item")) {
+    if (confirm("Are You Sure?")) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+// Clear Tasks
+function clearTasks() {
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// FilterTasks
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  // Change to matching start of text
+  document.querySelectorAll(".collection-item").forEach(task => {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) !== -1) {
+      task.style.display = "block";
+    } else {
+      task.style.display = "none";
+    }
+  });
 }
