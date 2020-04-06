26baec6486b4aff162824fdb295051132ed6e766
Kevin <kevin.m.guertin@gmail.com>
2020-04-05 21:04:57 -0400

    add gig model and first route

--- node-sequelize-demo/app.js
+++ node-sequelize-demo/app.js
@@ -3,18 +3,8 @@ const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const path = require("path");
 
-const Sequelize = require("sequelize");
-const db = new Sequelize("codegig", "postgres", "123456", {
-  host: "localhost",
-  port: 5433,
-  dialect: "postgres",
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-});
+// Database
+const db = require("./config/database");
 
 // Test DB
 db.authenticate()
@@ -24,6 +14,9 @@ db.authenticate()
 const app = express();
 
 app.get("/", (req, res) => res.send("INDEX"));
+
+// Gig Routes
+app.use("/gigs", require("./routes/gigs"));
 
 const PORT = process.env.PORT || 5000;
 
