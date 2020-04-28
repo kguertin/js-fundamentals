bfcea744728d532924adf2d17b665871fd97f778
Kevin <kevin.m.guertin@gmail.com>
2020-04-28 12:29:07 -0400

    add ability to create book and add to list through UI and Book constructors

---
+++ oop-bookstore-tutorial/app.js
@@ -0,0 +1,56 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+
+function UI() { }
+
+// Add book to list
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById('book-list');
+  const row = document.createElement('tr');
+  row.innerHTML = `
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href='#' class='delete'>X</a></td>
+  `
+  list.appendChild(row)
+}
+
+// Clear fields
+
+UI.prototype.clearFields = function () {
+  document.getElementById('title').value = ''
+  document.getElementById('author').value = ''
+  document.getElementById('isbn').value = ''
+}
+
+// Event Listeners
+
+document.getElementById('book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+
+  // Get form values
+  const title = document.getElementById('title').value,
+    author = document.getElementById('author').value,
+    isbn = document.getElementById('isbn').value
+
+  // Instantiate Book
+  const book = new Book(title, author, isbn);
+
+  //Instantiate UI
+  const ui = new UI()
+
+  // Add book to list
+  ui.addBookToList(book)
+
+  // Clear Fields
+  ui.clearFields()
+
+})
