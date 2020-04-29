7dfa75d0afe2ae7ac0d3be48d4785f1e53b1bc12
Kevin <kevin.m.guertin@gmail.com>
2020-04-29 17:19:58 -0400

    build oop xhr get function

---
+++ async-js/xhr-basics/http-library/easyhttp.js
@@ -0,0 +1,25 @@
+function easyHTTP() {
+  this.http = new XMLHttpRequest()
+}
+
+// Make an http get request
+easyHTTP.prototype.get = function (url, callback) {
+  this.http.open('GET', url, true)
+
+  let self = this;
+  this.http.onload = function () {
+    if (self.http.status === 200) {
+      callback(null, self.http.responseText)
+    } else {
+      callback('Error: ' + self.http.status)
+    }
+  }
+
+  this.http.send()
+}
+
+//Make an http post request
+
+//Make an http put request
+
+//Make an http delete request
