54fc07b1ec97b24bd062e034ba70f7f57b30d65e
Kevin <kevin.m.guertin@gmail.com>
2020-04-28 21:41:46 -0400

    use xhr to render text on page

--- async-js/xhr-basics/xhr.js
+++ async-js/xhr-basics/xhr.js
@@ -7,11 +7,27 @@ function loadData() {
   //Open
   xhr.open("GET", 'data.txt', true);
 
+  // Optional = used for spinners and loaders
+  xhr.onprogress = function () {
+    console.log('READYSTATE', xhr.readyState)
+  }
+
+  // console.log('READYSTATE', xhr.readyState)
+
   xhr.onload = function () {
     if (this.status === 200) {
-      console.log(this.responseText)
-
+      document.getElementById('output').innerHTML = `<h1>${this.responseText}</h1>`
     }
+  }
+
+  // xhr.onreadystatechange = function () {
+  //   if (this.status === 200 && this.readyState === 4) {
+  //     console.log(this.responseText)
+  //   }
+  // }
+
+  xhr.onerror = function () {
+    console.log('Request Error...')
   }
 
   xhr.send()
