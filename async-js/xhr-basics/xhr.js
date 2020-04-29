093fd7d2148e8ee5a27ba9d05670f8c8ed5dae51
Kevin <kevin.m.guertin@gmail.com>
2020-04-28 21:34:56 -0400

    set up basic xhr open

---
+++ async-js/xhr-basics/xhr.js
@@ -0,0 +1,18 @@
+document.getElementById('button').addEventListener('click', loadData);
+
+function loadData() {
+  // Create an XHR Object
+  const xhr = new XMLHttpRequest();
+
+  //Open
+  xhr.open("GET", 'data.txt', true);
+
+  xhr.onload = function () {
+    if (this.status === 200) {
+      console.log(this.responseText)
+
+    }
+  }
+
+  xhr.send()
+}
