72786c45e5c07072f60bb6b65bc9273a82faf7f6
Kevin <kevin.m.guertin@gmail.com>
2020-05-01 12:00:42 -0400

    add access to user repos

--- api-projects/github-finder/github.js
+++ api-projects/github-finder/github.js
@@ -2,15 +2,22 @@ class Github {
   constructor() {
     this.client_id = 'e519ce51f6ecbf1410f1';
     this.client_secret = '1cfedf217ea656302ddd2a97f3c6845a80c6cf7e';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json()
 
     return {
-      profile
+      profile,
+      repos
     }
   }
+
 }
