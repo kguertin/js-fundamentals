ed30b6524b89f21423cf608b7c3ba5d99e61eb31
Kevin <kevin.m.guertin@gmail.com>
2020-05-05 18:57:51 -0400

    test basic regex functions

---
+++ error-handling-and-regex/regex-fundamentals/app.js
@@ -0,0 +1,36 @@
+let re;
+re = /hello/;
+re = /hello/i; // i = case insensitive
+// re = /hello/g; // Global Search
+
+// console.log(re);
+// console.log(re.source);
+
+// exec() - return result in an array or null
+
+// const result = re.exec('hello world');
+
+// console.log(result);
+// console.log(result[0]);
+// console.log(result.index);
+// console.log(result.input)
+
+// test() -Returns true or false
+// const result = re.test('Hello')
+// console.log(result)
+
+// match() - return result array or null
+// const str = 'Hello There'
+// const result = str.match(re)
+// console.log(result)
+
+//search() - returns the index of the first match and if not found returns -1
+// const str = 'Well Hello There';
+// const result = str.search(re)
+// console.log(result)
+
+// replace() - return a new string with some or all matches of a pattern
+const string = 'Hello there';
+const newString = string.replace(re, 'Hi');
+console.log(newString)
+
