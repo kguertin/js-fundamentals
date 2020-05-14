f8dbf4864cc2bd55fa9a8e293f2efc074609c242
Kevin <kevin.m.guertin@gmail.com>
2020-05-14 15:52:06 -0400

    js module pattern using IIFE

---
+++ js-patterns/module-pattern/app.js
@@ -0,0 +1,54 @@
+// Basic Structure
+
+// (function () {
+//   //declare private vars and functions
+
+//   return {
+//     //Declare public var and functions
+//   }
+// })();
+
+// Standard Module Pattern 
+const UICtrl = (function () {
+  let text = 'Hello World';
+
+  const changeText = () => {
+    const element = document.querySelector('h1');
+    element.textContent = text
+  }
+
+  return {
+    callChangeText: function () {
+      changeText()
+      console.log(text)
+    }
+  }
+})();
+
+//Can only access public functions that were returned
+UICtrl.callChangeText();
+
+//Revealing Module Pattern 
+const ItemCtrl = (function () {
+  let data = [];
+
+  function add(item) {
+    data.push(item);
+    console.log('Item Added...')
+  };
+
+  function get(id) {
+    return data.find(item => item.id === id);
+  };
+
+  return {
+    add: add,
+    get: get
+  }
+})();
+
+
+ItemCtrl.add({ id: 1, name: 'John' });
+ItemCtrl.add({ id: 2, name: 'Mark' });
+console.log(ItemCtrl.get(1));
+console.log(ItemCtrl.get(2));
